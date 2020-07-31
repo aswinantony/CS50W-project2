ad40a2fb19e6d12eb3f0aa21ce3b4c39cdbf7b6c
Aswin Antony <aswinantony@gmail.com>
2020-07-31 09:48:20 +0530

    Sign in route updated and logout route created

--- application.py
+++ application.py
@@ -1,12 +1,17 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Keep track of channels created (Check for channel name)
+channelsCreated = []
+
+# Keep track of users logged (Check for username)
+usersLogged = []
 
 @app.route("/")
 def index():
@@ -14,7 +19,53 @@ def index():
 
 @app.route("/signin", methods=['GET','POST'])
 def signin():
-    return render_template("signin.html")
+    ''' Save the username on a Flask session 
+    after the user submit the sign in form '''
 
-# export FLASK_DEBUG=1
-# export FLASK_APP=application.py
+    # Forget any username
+    session.clear()
+
+    username = request.form.get("username")
+
+    if request.method == "POST":
+
+        if len(username) < 1 or username is '':
+            return render_template("error.html", message="username can't be empty.")
+
+        if username in usersLogged:
+            return render_template("error.html", message="that username already exists!")                   
+        
+        usersLogged.append(username)
+
+        session['username'] = username
+
+        # Remember the user session on a cookie if the browser is closed.
+        session.permanent = True
+
+        return redirect("/")
+
+    else:
+        return render_template("signin.html")
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    """ Logout user from list and delete cookie."""
+
+    # Remove from list
+    try:
+        usersLogged.remove(session['username'])
+    except ValueError:
+        pass
+
+    # exception ValueError is raised when an operation or function receives an argument that has the right type but an inappropriate value, 
+    # and the situation is not described by a more precise exception such as IndexError.
+
+    # Delete cookie
+    session.clear()
+
+    return redirect("/")
+
+# @app.route("/channels/", methods=['GET','POST'])
+# def enter_channel(channel):
+#     return render_template("channel.html")
+
