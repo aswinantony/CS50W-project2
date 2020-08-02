61c2715058f139ee27b3bb7cf14e600396e3bd40
aswinantony <aswinajraj@gmail.com>
2020-08-02 20:21:28 +0530

    decorator for checking login

---
+++ login_decorator.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from flask import redirect, render_template, request, session
+
+def login_required(f):
+    """
+    Login Required Decorator - This will make sure the user is logged in before continuing if used correctly in the required route.
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/signin")
+        return f(*args, **kwargs)
+    return decorated_function
